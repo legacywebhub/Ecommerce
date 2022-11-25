c8eb1c8be68c8817d75151e4072f89a3407d2647
legacywebhub <legacywebhub@gmail.com>
2022-11-25 22:08:44 +0100

    Live settings commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bootshop.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=list)
 
 
 # Application definition
@@ -89,6 +89,14 @@ AUTH_USER_MODEL = 'Store.MyUser'
 
 DATABASES = {
     'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
